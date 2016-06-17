f2ac7977cb604e7326bdec23aa29ee312f3aa6af
icelucio <lucio@lucios-mac.local>
2016-06-17 16:15:31 -0700

    Initial Commit

---
+++ Clase3/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Clase3
+//
+//  Created by Lucio on 6/17/16.
+//  Copyright © 2016 Lucio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Clase3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clase3
+//
+//  Created by Lucio on 6/17/16.
+//  Copyright © 2016 Lucio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
